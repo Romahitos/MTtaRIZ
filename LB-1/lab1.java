57092223589b93a93e80806ee528e14919a442d7
Roman Osadchiy <144036082+Romahitos@users.noreply.github.com>
2023-09-04 18:30:51 +0300

    Update lab1.java

--- LB-1/lab1.java
+++ LB-1/lab1.java
@@ -2,6 +2,7 @@ public class lab1 {
     public static void main(String[] args) {
 
         System.out.println( "Its wotks");
-        
+        System.out.println( "Its real wotks");
+
         }
 }
