76a383ed4f859dfbebb1f365dc375b31feed4ca0
Roman Osadchiy <144036082+Romahitos@users.noreply.github.com>
2023-09-12 09:49:49 +0300

    Update Player.java

--- LB-3/Player.java
+++ LB-3/Player.java
@@ -1,1 +1,30 @@
+import java.util.Scanner;
+
+public class Player {
+    public String name;
+    public char symbol;
+
+    public Player(String name, char symbol) {
+        this.name = name;
+        this.symbol = symbol;
+    }
+
+    public int[] makeMove() {
+        int[] move = new int[2];
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(name + ", введiть номер рядка (0, 1, 2): ");
+        move[0] = scanner.nextInt();
+
+        System.out.println(name + ", введiть номер стовпця (0, 1, 2): ");
+        move[1] = scanner.nextInt();
+
+        return move;
+    }
+
+    public void displayInfo() {
+        System.out.println("Гравець: " + name);
+        System.out.println("Символ: " + symbol);
+    }
+}
 
