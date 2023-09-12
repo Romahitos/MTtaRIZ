c8f77a1e49e9aa93a1d3ca050c2f3ac38f0af04e
Roman Osadchiy <144036082+Romahitos@users.noreply.github.com>
2023-09-12 09:49:27 +0300

    Update GameBoard.java

--- LB-3/GameBoard.java
+++ LB-3/GameBoard.java
@@ -1,1 +1,61 @@
+public class GameBoard {
+    private char[][] board; 
+
+    public GameBoard() {
+        board = new char[3][3]; 
+        initializeBoard();
+    }
+
+    public void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = ' '; 
+            }
+        }
+    }
+
+    public void displayBoard() {
+        System.out.println("-------------");
+        for (int i = 0; i < 3; i++) {
+            System.out.print("| ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j] + " | ");
+            }
+            System.out.println("\n-------------");
+        }
+    }
+
+    public boolean makeMove(int row, int col, char symbol) {
+        if (row >= 0 && row < 3 && col >= 0 && col < 3 && board[row][col] == ' ') {
+            board[row][col] = symbol;
+            return true; 
+        }
+        return false; 
+    }
+
+    public boolean checkWin(char symbol) {
+
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) {
+                return true; 
+            }
+            if (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol) {
+                return true; 
+            }
+        }
+
+        if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) {
+            return true;
+        }
+        if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol) {
+            return true;
+        }
+
+        return false; 
+    }
+
+    public char[][] getBoard() {
+        return board;
+    }
+}
 
